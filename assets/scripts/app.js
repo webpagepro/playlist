26ad504a7e278ef0a459521e477715bd45bb9b17
webpagepro <samuel@webpagepro.local>
2018-10-04 16:11:53 -0700

    layout revisions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 console.log("working working");
-
+const baseURL = 'https://lit-fortress-6467.herokuapp.com/object'
 const leftCol = document.querySelector('.left-col');
 const rightCol = document.querySelector('.right-col');
 
-rightCol.style.background = "images/fadedTrack.jpg";
+const image = document.querySelector(".role-preview");
+const image = document.querySelector(".images-sidebar");
