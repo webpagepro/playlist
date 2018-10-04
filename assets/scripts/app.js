4657665f6e689af8d626cc4fcf5d5bf7b775f548
webpagepro <samuel@webpagepro.local>
2018-10-04 13:45:34 -0700

    second commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,6 @@
 console.log("working working");
+
+const leftCol = document.querySelector('.left-col');
+const rightCol = document.querySelector('.right-col');
+
+rightCol.style.background = "images/fadedTrack.jpg";
