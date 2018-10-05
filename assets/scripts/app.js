ad337c1775b733efef543077025d03938a412c7e
webpagepro <samuel@webpagepro.local>
2018-10-05 15:26:46 -0700

    final submit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,3 @@
-console.log("working too");
 const baseURL = 'https://lit-fortress-6467.herokuapp.com/object'
 
 const carousel = document.querySelector('.carousel');
@@ -24,12 +23,36 @@ fetch(baseURL)
   .then(function(jsonData) {
 //console.log(jsonData);
 let newObj = jsonData.results;
+console.log(newObj);
 
-//  for (let k in newObj) {
-console.log(Object.keys(newObj));
 
-  // albumDetails.innerHTML = '<p>jsonData}</p>';
+  /* albumDetails.innerHTML = '<p>jsonData}</p>';
 
-  //
+  0
+:
+{id: 2, title: "Ghost in the Machine", artist: "The Police", cover_art: "ghost_in_the_machine.jpg"}
+1
+:
+{id: 6, title: "Red", artist: "Black Uhuru", cover_art: "red.jpg"}
+2
+:
+{id: 22, title: "The Division Bell", artist: "Pink Floyd", cover_art: "the_division_bell.jpg"}
+3
+:
+{id: 18, title: "Thriller", artist: "Michael Jackson", cover_art: "thriller.jpg"}
+4
+:
+{id: 55, title: "21", artist: "Adele", cover_art: "21.jpg"}
+*/
+
+for (let k in newObj) {
+    console.log(newObj[k]);
+
+const info = document.createElement('div');
+info.className ="album-info";
+info.innerHTML = newObj[k].title;
+albumDetails.appendChild(info);
+
+}
 
   });
