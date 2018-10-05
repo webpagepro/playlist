d9b75b8f2784d2be597248fd6578d5987c7a7d4e
webpagepro <samuel@webpagepro.local>
2018-10-05 14:09:54 -0700

    backup

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,28 +1,35 @@
-
+console.log("working too");
 const baseURL = 'https://lit-fortress-6467.herokuapp.com/object'
-const leftCol = document.querySelector('.left-col');
-const rightCol = document.querySelector('.right-col');
 
-const imgSidebar = document.querySelector('.images-sidebar');
-const imgPrev = document.querySelector('.img-preview');
-
-let myPix = ["images/thriller.jpg","images/ghost_in_the_machine.jpg","images/red.jpg","images/21.jpg","images/the_division_bell.jpg"];
-
-let myPix2 = ["thriller.jpg","ghost_in_the_machine.jpg","red.jpg","21.jpg","the_division_bell.jpg"];
-
-for(let i = 0; i < myPix.length; i++){
-const img = document.createElement('img');
-img.className ="discography";
-img.src = myPix2[i];
-imgPrev.appendChild(img);
-
-}
-
+const carousel = document.querySelector('.carousel');
+const albumDetails = document.querySelector('.album-details');
+/*
+{id: 2, title: "Ghost in the Machine", artist: "The Police", cover_art: "ghost_in_the_machine.jpg"},
+{id: 6, title: "Red", artist: "Black Uhuru", cover_art: "red.jpg"},
+{id: 22, title: "The Division Bell", artist: "Pink Floyd", cover_art: "the_division_bell.jpg"},
+{id: 18, title: "Thriller", artist: "Michael Jackson", cover_art: "thriller.jpg"},
+{id: 55, title: "21", artist: "Adele", cover_art: "21.jpg"};
+*/
+//console.log(obj);
 
 //API Request
-fetch(baseURL).then ((res) => {
-let album = document.querySelector('.img-preview');
+fetch(baseURL)
+  // once we have our data clean it up for us
+  .then(function(res) {
 
-image.src = 'images/thriller,jpg';
+    // turn readable stream into json (javascript object notation)
+    return res.json();
+  })
+  // once data is turned into json do something else
+  .then(function(jsonData) {
+//console.log(jsonData);
+let newObj = jsonData.results;
+;
+//  for (let k in newObj) {
+console.log(Object.keys(newObj));
 
-}); //END Fetch
+  // albumDetails.innerHTML = '<p>jsonData}</p>';
+
+  //
+
+  });
